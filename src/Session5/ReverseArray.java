740483a131a0d35220137e29d4b71a4811e8e8d9
fedyaky <fedyaky2020@gmail.com>
2025-01-15 19:25:31 -0800

    not first update

--- src/Session5/ReverseArray.java
+++ src/Session5/ReverseArray.java
@@ -5,8 +5,8 @@ public class ReverseArray {
         int [] numbers = {10,20,30,40,50};
 
         System.out.println("Original array:");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+        for (int number : numbers) { //where number is the local variable
+            System.out.print(number + " ");
         }
         System.out.println();
 
@@ -14,8 +14,6 @@ public class ReverseArray {
         for (int i = numbers.length - 1; i >= 0; i--) {
             System.out.print(numbers[i] + " ");
         }
-
-
 
 //        System.out.println("Original array: ");
 //        for (int i = 0; i < numbers.length; i++) {
