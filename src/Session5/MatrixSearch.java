5ac8902d02988ba09d36646b64fc615af5c46f25
fedyaky <fedyaky2020@gmail.com>
2025-01-15 23:01:59 -0800

    not first update

--- src/Session5/MatrixSearch.java
+++ src/Session5/MatrixSearch.java
@@ -6,16 +6,17 @@ import java.util.Scanner;
 public class MatrixSearch {
     public static void main(String[] args) {
         Random r = new Random();
-        for (int i = 0; i < 10; i++) {
-//            System.out.println(r.nextInt(20));
+        for (int i = 0; i < 500; i++) {
+//            System.out.print(r.nextInt(20) + " ");
         }
+        System.out.println();
     int[][] matrix = new int[4][4];
 //        Random r = new Random();
 
         // Fill the matrix with random integers
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                matrix[i][j] = r.nextInt(20); // Random integers between 0 and 20
+                matrix[i][j] = r.nextInt(500); // Random integers between 0 and 20
             }
         }
 
@@ -52,5 +53,11 @@ public class MatrixSearch {
         }
 
         scanner.close();
+
+        // Check if the number was found in the matrix or not found at all
+        boolean found = false;
+        if (!found) {
+            System.out.println("Number not found in the matrix");
+        }
     }
 }
